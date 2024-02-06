d911a31e72d203f44e5ea4c52b1d8d466ea31146
Charles Buckman <buckmanc@gmail.com>
2024-02-05 23:10:00 -0600

    outline icons, add .icos because windows can't handle pngs

---
+++ .config/assets/icons/make_icos.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+pngDir="$HOME/.config/assets/icons/png"
+icoDir="$HOME/.config/assets/icons/ico"
+
+if [[ -d "$icoDir" ]]
+then
+	rm -r "$icoDir"
+fi
+
+mkdir -p "$icoDir"
+
+for inPath in "$pngDir"/*.png
+do
+	filename="$(basename "$inPath")"
+	filename="${filename%.*}"
+	outPath="${icoDir}/${filename}.ico"
+
+	# echo "inPath: $inPath"
+	# echo "outPath: $outPath"
+
+	convert "$inPath" "$outPath"
+done
+
+echo "$(ls "$pngDir/"*.png | wc -l) pngs"
+echo "$(ls "$icoDir/"*.ico | wc -l) icos"
+
--- .config/assets/icons/prefill.sh
+++
@@ -1,2 +0,0 @@
-wallpaper-magick -i "/media/content/Dropbox/terminal_wallpapers/input icons/bucket_thicc.png" -o "/media/content/Dropbox/terminal_wallpapers/output icons/bucket_thicc.png" -m icon -a
-wallpaper-magick -i "/media/content/Dropbox/terminal_wallpapers/input icons/bucket_thin.png" -o "/media/content/Dropbox/terminal_wallpapers/output icons/bucket_thin.png" -m icon -a
